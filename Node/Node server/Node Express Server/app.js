eb1b4a4a00b16142b52669a0c80ce68da9ef4f0a
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2023-07-15 01:02:38 +0300

    Update app.js

--- Node/Node server/Node Express Server/app.js
+++ Node/Node server/Node Express Server/app.js
@@ -14,10 +14,27 @@ app.use(function(req, res, next){
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+
+// Navigations
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.send('Hello World!');
 });
+
+app.get('/About', function(req, res){
+    res.send('About Page.');
+});
+
+app.get('/Contact', function(req, res){
+    res.send('Contact Page.');
+});
+
+app.get('/Forum', function(req, res){
+    res.send('Forum Page.');
+});
+
+// Navigations
 
 app.listen(port);
 console.log('Server started on port '+port);
