2f5131e535309a58712abe49163e46cecd2e448d
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2023-09-28 19:44:50 +0300

    Delete htmldb directory

--- htmldb/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connect to mySQL
-$conn = mysqli_connect('localhost', '<user>', '<PASSWORD>', '<DB>');
-
-// If connection failed
-if(mysqli_connect_errno()){
-    echo 'DB Connection failed...', mysqli_connect_error;
-}
-?>
