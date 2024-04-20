7f1276857b915c85459b7b5876473312505cdc76
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2024-04-20 03:33:15 +0300

    Update config.php

--- PHP/Simple search BAR/config.php
+++ PHP/Simple search BAR/config.php
@@ -2,7 +2,6 @@
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" href="style.css">
-    <script src="scripts.js"></script>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
