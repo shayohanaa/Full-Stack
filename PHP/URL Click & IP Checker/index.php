b9c0c2d45e2d865da4e9207dacdad791319879fe
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2024-06-27 02:59:05 +0300

    Update index.php

--- PHP/URL Click & IP Checker/index.php
+++ PHP/URL Click & IP Checker/index.php
@@ -14,29 +14,24 @@ function getUserIP() {
 if (isset($_GET['link_id']) && $_GET['link_id'] != '') {
     $link_id = mysqli_real_escape_string($conn, $_GET['link_id']);
 
-    // Check if the IP has already clicked the link
     $ip = getUserIP();
     $checkifClicked = "SELECT * FROM clicked WHERE linkID = '$link_id' AND ip = '$ip'";
     $checkIP = mysqli_query($conn, $checkifClicked);
 
     if (mysqli_num_rows($checkIP) == 0) {
-        // Query to check if the link exists
         $query = "SELECT * FROM linkclicks WHERE linkID = '$link_id'";
         $result = mysqli_query($conn, $query);
 
-        if (!$result || mysqli_num_rows($result) == 0) { // If no results found
+        if (!$result || mysqli_num_rows($result) == 0) {
             header('Location: index.php');
-            exit; // Terminate the script after redirection
+            exit;
         } else {
             $row = mysqli_fetch_assoc($result);
             $clicks = $row['clicks'];
-            $clicks++; // Increment the click count
+            $clicks++;
 
-            // Update the clicks in the database
             $update_query = "UPDATE linkclicks SET clicks = $clicks WHERE linkID = '$link_id'";
             if (mysqli_query($conn, $update_query)) {
-                echo 'yes results';
-                // Insert IP into clicked table
                 $insertIP = "INSERT INTO clicked (ip, linkID) VALUES('$ip', '$link_id')";
                 mysqli_query($conn, $insertIP);
             } else {
@@ -44,8 +39,8 @@ if (isset($_GET['link_id']) && $_GET['link_id'] != '') {
             }
         }
     } else {
-        header('Location: index.php?IP-EXISTS');
-        exit; // Terminate the script after redirection
+        header('Location: index.php'); 
+        exit; 
     }
 }
 ?>
