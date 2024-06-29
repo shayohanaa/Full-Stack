61e2ea6d24a30ecb1bc628df802b44395c36419b
Shay ohana <91027583+shayohanaa@users.noreply.github.com>
2024-06-29 17:15:43 +0300

    Delete PHP/Registration System directory

--- PHP/Registration System/Assets/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connect to mySQL
-$conn = mysqli_connect('localhost', 'root', '', 'DB');
-
-// If connection failed
-if(mysqli_connect_errno()){
-    echo 'DB Connection failed...', mysqli_connect_error;
-}
-?>
